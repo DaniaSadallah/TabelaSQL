2e0c67d449fc80b282be099a5834d9e0c3f4867a
Dania Sadallah Kader <daniasadallah@gmail.com>
2025-03-20 21:24:54 -0300

    Update SQLQuery1 20.03.sql

--- Exercicios/SQLQuery1 20.03.sql
+++ Exercicios/SQLQuery1 20.03.sql
@@ -1,12 +1,1 @@
-select * from tb_cargo where cd_cargo = 1;
-
-delete from dbo.tb_cargo where cd_cargo = 1;
-
-ALTER TABLE tb_cargo NOCHECK CONSTRAINT ALL;
-DELETE FROM tb_cargo WHERE cd_cargo = 1;
-ALTER TABLE tb_cargo CHECK CONSTRAINT ALL;
-
-ALTER TABLE tb_funcionario DROP CONSTRAINT fk_cargo;
-DELETE FROM tb_cargo WHERE cd_cargo = 1;
-ALTER TABLE tb_funcionario ADD CONSTRAINT fk_cargo FOREIGN KEY (cd_cargo) REFERENCES tb_cargo(cd_cargo);
 
